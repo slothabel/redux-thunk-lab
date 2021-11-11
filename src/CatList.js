860f67d69ed58f3ae093e7e6193aaf50ee6412d7
Yoshi <yoshimejia00@gmail.com>
2021-11-11 08:55:43 -0500

    commit tree bb88adada6db43a931706e5ecb657a68294a7f38

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,16 @@
-// write your CatList component here
+// import { render } from "enzyme";
+import React from "react";
+
+export default class CatList extends React.Component {
+
+    render(){
+        const cats = this.props.catPics.map((cat) => <li key={cat.id}><img alt="cat pic"  src={cat.url} /></li>)
+        // debugger
+        console.log('cats')
+        return(
+            <div>{cats}</div>
+            
+        )
+        
+    }
+}
